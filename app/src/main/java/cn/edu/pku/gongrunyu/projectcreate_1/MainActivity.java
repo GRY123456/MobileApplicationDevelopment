714c53b5b24dba5749467052f8f22e2f86c5a490
GRY123456 <876767157@qq.com>
2018-09-26 10:50:38 +0800

    third commit

---
+++ app/src/main/java/cn/edu/pku/gongrunyu/projectcreate_1/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.pku.gongrunyu.projectcreate_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
