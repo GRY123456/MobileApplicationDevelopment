1088ea1c3f628e7448362054a8773660eebfa6d6
GRY123456 <876767157@qq.com>
2018-10-09 21:05:10 +0800

    4 CheckNetState

--- app/src/main/java/cn/edu/pku/gongrunyu/projectcreate_1/MainActivity.java
+++ app/src/main/java/cn/edu/pku/gongrunyu/projectcreate_1/MainActivity.java
@@ -2,6 +2,10 @@ package cn.edu.pku.gongrunyu.projectcreate_1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.edu.pku.gongrunyu.util.NetUtil;
 
 
 public class MainActivity extends Activity {
@@ -10,5 +14,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("myWeather","网络OK了！");
+            Toast.makeText(MainActivity.this,"网络OK!",Toast.LENGTH_LONG).show();
+        }
+        else {
+            Log.d("myWeather","网络gg了！");
+            Toast.makeText(MainActivity.this,"网络gg了！",Toast.LENGTH_LONG).show();
+        }
     }
 }
