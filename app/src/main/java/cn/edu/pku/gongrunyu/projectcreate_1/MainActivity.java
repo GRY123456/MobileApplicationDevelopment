fc37ee606556b58d196f20bf74aaa6a58d092e86
GRY123456 <876767157@qq.com>
2018-10-16 20:17:52 +0800

    8 SelectCity

--- app/src/main/java/cn/edu/pku/gongrunyu/projectcreate_1/MainActivity.java
+++ app/src/main/java/cn/edu/pku/gongrunyu/projectcreate_1/MainActivity.java
@@ -1,6 +1,7 @@
 package cn.edu.pku.gongrunyu.projectcreate_1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
@@ -31,6 +32,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
     private static final int UPDATE_TODAY_WEATHER = 1;
 
     private ImageView mUpdateBtn;
+
+    private ImageView mCitySelect;
 
     private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv,
             temperatureTv, climateTv, windTv, city_name_Tv;
@@ -67,6 +70,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
             Toast.makeText(MainActivity.this,"网络gg了！",Toast.LENGTH_LONG).show();
         }
 
+        mCitySelect = (ImageView) findViewById(R.id.title_city_manager);
+        mCitySelect.setOnClickListener(this);
+
         initView();
     }
 
@@ -100,6 +106,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
+
+        if(view.getId() == R.id.title_city_manager){
+            Intent i = new Intent(this,SelectCity.class);
+            startActivity(i);
+        }
 
         if(view.getId() == R.id.title_update_btn){
             SharedPreferences sharedPreferences = getSharedPreferences("config",MODE_PRIVATE);
---
+++ app/src/main/java/cn/edu/pku/gongrunyu/projectcreate_1/SelectCity.java
@@ -0,0 +1,14 @@
+package cn.edu.pku.gongrunyu.projectcreate_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectCity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.select_city);
+    }
+}
