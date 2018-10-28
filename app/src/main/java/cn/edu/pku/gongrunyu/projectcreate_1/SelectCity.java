25b756079bbced22622c732659550fabe3069864
GRY123456 <876767157@qq.com>
2018-10-28 15:48:51 +0800

    10and11 ShowCity ClickCity

--- app/src/main/java/cn/edu/pku/gongrunyu/projectcreate_1/SelectCity.java
+++ app/src/main/java/cn/edu/pku/gongrunyu/projectcreate_1/SelectCity.java
@@ -4,22 +4,61 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import cn.edu.pku.gongrunyu.app.MyApplication;
+import cn.edu.pku.gongrunyu.bean.City;
 
 public class SelectCity extends Activity implements View.OnClickListener{
 
     private ImageView mBackBtn;
+    private ListView mList;
+    private List<City> cityList;
+    private String[] data;
+    private ArrayAdapter<String> myadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.select_city);
 
+        initViews();
+    }
+
+    private void initViews(){
+        //为mBackBtn设置监听事件
         mBackBtn = (ImageView) findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
 
+        //mClearEditText = (ClearEditText) findViewById(R.id.search_city);
 
+        mList = (ListView) findViewById(R.id.title_list);
+        MyApplication myApplication = (MyApplication) getApplication();
+        cityList = myApplication.getCityList();
+
+        data = new String[cityList.size()];
+        for (int i=0;i<cityList.size();i++){
+            data[i] = cityList.get(i).getCity()
+                    + " " + cityList.get(i).getNumber();
+        }
+
+        myadapter = new ArrayAdapter<String>(SelectCity.this,
+                android.R.layout.simple_list_item_1,data);
+        mList.setAdapter(myadapter);
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SelectCity.this,
+                        "CityCode"+ cityList.get(position).getNumber(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
